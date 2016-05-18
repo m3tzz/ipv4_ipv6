c2eb280042a1bcc5d6d1a04396a0671263f677ba
Ruben Barbosa <ruben.barbosa@blip.pt>
2016-05-18 23:29:11 +0100

    first commit

---
+++ ip.py
@@ -0,0 +1,105 @@
+import sys
+import re
+
+#  \b                                           # matches the beginning of the string
+#   (25[0-5]|                                    # matches the integer range 250-255 OR
+#   2[0-4][0-9]|                                 # matches the integer range 200-249 OR
+#   [01]?[0-9][0-9]?)                            # matches any other combination of 1-3 digits below 200
+#   \.                                           # matches '.'
+#   (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)       # repeat
+#   \.                                           # matches '.'
+#   (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)       # repeat
+#   \.                                           # matches '.'
+#   (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)       # repeat
+#   \b                                           # matches the end of the string
+
+def is_valid_ip(ip):
+    """Validates IP addresses.
+    """
+    return is_valid_ipv4(ip) or is_valid_ipv6(ip)
+
+def is_valid_ipv4(ip):
+    """Validates IPv4 addresses.
+    """
+    pattern = re.compile(r"""
+        ^
+        (?:
+          # Dotted variants:
+          (?:
+            # Decimal 1-255 (no leading 0's)
+            [3-9]\d?|2(?:5[0-5]|[0-4]?\d)?|1\d{0,2}
+          |
+            0x0*[0-9a-f]{1,2}  # Hexadecimal 0x0 - 0xFF (possible leading 0's)
+          |
+            0+[1-3]?[0-7]{0,2} # Octal 0 - 0377 (possible leading 0's)
+          )
+          (?:                  # Repeat 0-3 times, separated by a dot
+            \.
+            (?:
+              [3-9]\d?|2(?:5[0-5]|[0-4]?\d)?|1\d{0,2}
+            |
+              0x0*[0-9a-f]{1,2}
+            |
+              0+[1-3]?[0-7]{0,2}
+            )
+          ){0,3}
+        |
+          0x0*[0-9a-f]{1,8}    # Hexadecimal notation, 0x0 - 0xffffffff
+        |
+          0+[0-3]?[0-7]{0,10}  # Octal notation, 0 - 037777777777
+        |
+          # Decimal notation, 1-4294967295:
+          429496729[0-5]|42949672[0-8]\d|4294967[01]\d\d|429496[0-6]\d{3}|
+          42949[0-5]\d{4}|4294[0-8]\d{5}|429[0-3]\d{6}|42[0-8]\d{7}|
+          4[01]\d{8}|[1-3]\d{0,9}|[4-9]\d{0,8}
+        )
+        $
+    """, re.VERBOSE | re.IGNORECASE)
+    print ip
+    print pattern.match(ip)
+    return pattern.match(ip) is not None
+
+def is_valid_ipv6(ip):
+    """Validates IPv6 addresses.
+    """
+    pattern = re.compile(r"""
+        ^
+        \s*                         # Leading whitespace
+        (?!.*::.*::)                # Only a single whildcard allowed
+        (?:(?!:)|:(?=:))            # Colon iff it would be part of a wildcard
+        (?:                         # Repeat 6 times:
+            [0-9a-f]{0,4}           #   A group of at most four hexadecimal digits
+            (?:(?<=::)|(?<!::):)    #   Colon unless preceeded by wildcard
+        ){6}                        #
+        (?:                         # Either
+            [0-9a-f]{0,4}           #   Another group
+            (?:(?<=::)|(?<!::):)    #   Colon unless preceeded by wildcard
+            [0-9a-f]{0,4}           #   Last group
+            (?: (?<=::)             #   Colon iff preceeded by exacly one colon
+             |  (?<!:)              #
+             |  (?<=:) (?<!::) :    #
+             )                      # OR
+         |                          #   A v4 address with NO leading zeros
+            (?:25[0-4]|2[0-4]\d|1\d\d|[1-9]?\d)
+            (?: \.
+                (?:25[0-4]|2[0-4]\d|1\d\d|[1-9]?\d)
+            ){3}
+        )
+        \s*                         # Trailing whitespace
+        $
+    """, re.VERBOSE | re.IGNORECASE | re.DOTALL)
+    print ip
+    print pattern.match(ip)
+    return pattern.match(ip) is not None
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        print"""
+        This app requires an argument example: ip.py IP Address
+        """
+        exit()
+    else:
+        ip_str = sys.argv[1]
+        print is_valid_ip(ip_str)
